a0cea4084e226b9f33288e0fbbe04dcd868c05e9
Igloopotato <141108687+Igloopotato@users.noreply.github.com>
2025-02-23 12:06:44 +0800

    answer 4. need to be revised later

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+ // input - an array with 1 or more value to be removed - [a,b,c,d]
+    // output - array with those values to be removed - [a,b,c]
+
+const removeFromArray = function(array, ...args) {
+   const newArray = [];
+   array.forEach((item) => {
+        if(!args.includes(item))
+        {
+            newArray.push(item);
+        }
+   });
+   return newArray;
 };
 
 // Do not edit below this line

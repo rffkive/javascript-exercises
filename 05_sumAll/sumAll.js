acb75feb835f3bfc9cca116d78758147e013b830
Igloopotato <141108687+Igloopotato@users.noreply.github.com>
2025-04-19 20:00:21 +0800

    submit all test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function(firstNumber,lastNumber) {
+        if ( firstNumber > lastNumber) {
+                let temp = firstNumber; 
+                firstNumber = lastNumber;
+                lastNumber = temp;
+        }
 
+        if ( firstNumber < 0 || lastNumber < 0) {
+                return "ERROR";
+        }
+
+        if (!Number.isInteger(firstNumber) || !Number.isInteger(lastNumber)) {
+                return "ERROR";
+        }
+        let totalNUmber = 0;
+
+        for ( i=firstNumber; i <= lastNumber; i++) {
+                totalNUmber += i;
+        } 
+        return totalNUmber;
 };
 
-// Do not edit below this line
 module.exports = sumAll;

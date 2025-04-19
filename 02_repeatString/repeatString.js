d515457ae996e51b5af07008bc8e4584c710a33f
Igloopotato <141108687+Igloopotato@users.noreply.github.com>
2025-04-19 15:00:39 +0800

    answer the rigt one

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let result = "";
+    for (i = 0; i < num; i++) {
+      result += string;}
+      return result;
 };
 
 // Do not edit below this line

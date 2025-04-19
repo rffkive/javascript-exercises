d196d191156d33e36cc48ab1ee7276d0fd7f7bf6
Igloopotato <141108687+Igloopotato@users.noreply.github.com>
2025-04-19 16:38:57 +0800

    solve the first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, num) {
+        const index = array.indexOf(num);
+
+        if (index > -1) { //only spliuce array when item is found
+                array.splice(index, 1); //2nd parameter means remove one item only
+        }
+            return array;
 };
 
 // Do not edit below this line

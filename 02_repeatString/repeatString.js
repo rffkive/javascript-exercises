1aa36d819e7b22883e25472701612efc2e58142d
Igloopotato <141108687+Igloopotato@users.noreply.github.com>
2025-04-19 14:02:53 +0800

    repeat the test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,10 +1,6 @@
-const repeatString = function (word, times){ // store 2 arguments as required
-    if (times < 3) return 'ERROR';  // if times = negative, return error
-    let string = ""; //declare empty string to store the result
-    for (i=0; i < times; i++) { // loop
-        string += word;} // concat the word to our string
-        return string; // return the string, "word" repeated how many "times"
-  };
+const repeatString = function() {
+
+};
 
 // Do not edit below this line
 module.exports = repeatString;

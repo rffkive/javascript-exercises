3cd427b8a22ec8cd5b2293c33329fcd096e1079f
Igloopotato <141108687+Igloopotato@users.noreply.github.com>
2025-04-19 16:48:35 +0800

    solve 7 out of 8 question

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,15 @@
-const removeFromArray = function(array, num) {
-        const index = array.indexOf(num);
-
-        if (index > -1) { //only spliuce array when item is found
-                array.splice(index, 1); //2nd parameter means remove one item only
-        }
-            return array;
-};
+const removeFromArray = function(array, ...num) 
+    {
+        for ( const number of num) 
+              {
+                const index = array.indexOf(number);
+                if (index > -1) 
+                  { //only spliuce array when item is found
+                     array.splice(index,1); //2nd parameter means remove one item only
+                  }
+              }
+        return array;
+    };
 
 // Do not edit below this line
 module.exports = removeFromArray;

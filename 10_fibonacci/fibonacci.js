5f326c362e6c2f5cb4557a37216102b61a535670
Igloopotato <141108687+Igloopotato@users.noreply.github.com>
2025-05-23 01:01:17 +0800

    pass the test

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(arg) {
+    let a = 0;
+    let b = 1;
+    let nexTerm;
 
-};
+    let num;
+    if ( typeof arg !== 'number' ) {
+        num = parseInt (arg);
+    }  else {num = arg;}
+
+    if (num == 0) 
+        { return 0;} 
+    else if ( num < 0) 
+        { return "OOPS" } 
+      
+    else {
+
+             for (let i=1; i<num; i++ ) {
+                nexTerm = a+b;
+                a = b;
+                b = nexTerm;
+            }
+
+            return b;}
+         };
 
 // Do not edit below this line
 module.exports = fibonacci;

64690f5147cc4c9582d613c0d947ea1ea5a0b9ac
Igloopotato <141108687+Igloopotato@users.noreply.github.com>
2025-05-23 01:41:12 +0800

    complete the test

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,9 @@
-const getTheTitles = function() {
-
+    const getTheTitles = function(bookTitle) {
+    return bookTitle.map( book => book.title)
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
+
+

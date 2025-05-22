5f9e2d29edda9f7737b26c8a5df402b9ebc668ef
Igloopotato <141108687+Igloopotato@users.noreply.github.com>
2025-05-23 04:07:18 +0800

    complete the task

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,41 @@
-const findTheOldest = function() {
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
+  
+  getAge = function (birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+  }
 
+  const findTheOldest = function(newArray) {
+    return newArray.reduce ((oldest, currentPerson) => {
+        const oldestAge = getAge(
+            oldest.yearOfBirth, 
+            oldest.yearOfDeath
+        );
+        const currentAge = getAge(
+          currentPerson.yearOfBirth,
+          currentPerson.yearOfDeath
+        );
+        return oldestAge < currentAge ? currentPerson : oldest;
+
+    })
 };
 
 // Do not edit below this line
